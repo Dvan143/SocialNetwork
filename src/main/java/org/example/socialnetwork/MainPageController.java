a61fea13bbd021580e690cdd07278d7c5effb9f5
Dvan143 <dvanish445@yandex.ru>
2024-11-16 19:16:29 +0300

    Renamed variables for java

--- src/main/java/org/example/socialnetwork/MainPageController.java
+++ src/main/java/org/example/socialnetwork/MainPageController.java
@@ -5,8 +5,32 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainPageController {
-    @GetMapping
+    @GetMapping("/")
     public String index() {
         return "index";
+    }
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+    @GetMapping("/auth")
+    public String auth() {
+        return "auth";
+    }
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
+    @GetMapping("/register")
+    public String register() {
+        return "register";
+    }
+    @GetMapping("/resetpassword")
+    public String resetpassword() {
+        return "resetpassword";
+    }
+    @GetMapping("settings")
+    public String settings() {
+        return "settings";
     }
 }
