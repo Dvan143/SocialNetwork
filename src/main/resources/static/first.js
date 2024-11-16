19c70021fb99723ca412f452f819c1d69460b208
Dvan143 <dvanish445@yandex.ru>
2024-11-16 18:57:38 +0300

    Added index.html and other pages

---
+++ src/main/resources/static/first.js
@@ -0,0 +1,22 @@
+document.getElementById('forma').addEventListener("submit", checkAuth);
+
+function checkAuth(event){
+    event.preventDefault();
+    var elem = document.getElementById('forma');
+    var name = elem.username.value;
+    var pass = elem.password.value;
+    var mail = elem.email.value;
+    var url = false;
+    var fail = "";
+    if(document.location.href.split("register.html").length > 1) url = true;
+    if(url) var apass = elem.accpassword.value;
+
+    // if(url){if(pass != apass) fail = "Пароли отличаются";}
+    if(url){if(pass != apass) fail = "Пароли отличаются";}
+    if(name == "" || pass == "" || mail == "") fail = "Заполните все поля";
+    else if(pass.split("&").length > 1) fail = "Некорректный пароль";
+    if(fail != "") document.getElementById('error').innerHTML = fail;
+    else{
+        window.location = 'index.html';
+    }
+}
