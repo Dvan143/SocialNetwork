8120fa21373ae702123e01c3ccc5f7580c546c88
Dvan143 <dvanish445@yandex.ru>
2024-12-30 12:38:10 +0300

    Added reset password function. Added email support

--- src/main/java/org/example/SocialNetwork/controllers/MainController.java
+++ src/main/java/org/example/SocialNetwork/controllers/MainController.java
@@ -29,11 +29,21 @@ public class MainController {
     @GetMapping("profile/{id}")
     public String hello(@PathVariable("id") int id, Model model) {
         model.addAttribute("userName", db.getUserById(id));
-        model.addAttribute("id", id);
+        model.addAttribute("email", db.getEmailById(id));
         return "profile";
     }
-    @PostMapping("resetPassword")
-    public String resetPassword(@RequestParam("email") String email, Model model) {
+    @GetMapping("resetPassword")
+    public String resetPassword(Model model) {
+        return "resetPassword";
+    }
+    @PostMapping("resetingPassword")
+    public String resetingPassword(@RequestParam("email") String email, Model model){
+        if(db.emailExists(email)){
+            db.resetPasswordByEmail(email);
+            model.addAttribute("info","Message has been sent");
+        } else {
+            model.addAttribute("info","Email does not exist");
+        }
         return "resetPassword";
     }
     @GetMapping("/register")
@@ -42,11 +52,14 @@ public class MainController {
     }
     @PostMapping("registerUser")
     public String register(@RequestParam("email") String email,@RequestParam("userName") String userName, @RequestParam("password") String password, Model model) {
-        if(!db.userExists(userName)){
-            db.newUser(userName, password);
-            model.addAttribute("info", "User registered successfully");
-        }else {
+        if(db.userExists(userName)){
             model.addAttribute("info","User already exists");
+        } else if (db.emailExists(email)) {
+            model.addAttribute("info","Email already exists");
+        } else {
+            model.addAttribute("info","User already exists");
+            db.newUser(email, userName, password);
+            model.addAttribute("info", "User registered successfully");
         }
         return "register";
     }
